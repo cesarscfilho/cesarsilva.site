bb6940f55b8f322679e59980d791b473dfa49ce3
Cesar Silva <cesarfilhoslv@gmail.com>
2023-08-23 19:19:24 -0300

    feat: balancer h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import { Icons } from "@/components/Icons";
 import { StarIcon } from "lucide-react";
 import Link from "next/link";
+import { Balancer } from "react-wrap-balancer";
 
 export default function Home() {
   return (
     <main className="max-w-6xl m-auto my-6 rounded-md flex flex-col md:flex-row">
-      <header className="flex w-[800px] flex-col self-start gap-8 pt-8">
+      <header className="flex flex-col self-start gap-8 md:w-[850px]">
         <div className="h-24 w-24 rounded bg-gradient-to-r from-purple-600 to-blue-600" />
         <div className="space-y-3">
-          <h1 className="text-4xl font-extrabold mb-0">
-            I&apos;m Cesar, a Front-end Developer.
+          <h1 className="text-3xl font-extrabold mb-0">
+            <Balancer>I&apos;m Cesar, a Front-end Developer.</Balancer>
           </h1>
           <p className="text-gray-500">
             I like to learn new technologies and create applications.
@@ -17,12 +18,12 @@ export default function Home() {
         </div>
       </header>
 
-      <section>
-        <h2 className="text-xl font-medium mb-1">Projects</h2>
+      <section className="mt-10">
+        <h2 className="text-2xl font-medium mb-5">Projects</h2>
         <ul className="md:grid lg:grid-cols-2 gap-4 flex flex-col mt-3">
           <li className="rounded-md border bg-white border-gray-300 list-none">
-            <div className="p-6 hover:border-gray-400  rounded-sm h-full">
-              <div className="flex flex-col justify-between h-full">
+            <div className="p-6 hover:border-gray-400 rounded-sm ">
+              <div className="flex flex-col justify-between">
                 <div>
                   <h4 className="text-lg font-medium">Lorem Ipsum</h4>
                   <p className="line-clamp-2">
