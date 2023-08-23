d59409ffe6c7ace3a7c0c6c8e0426f73ce360d32
Cesar Silva <cesarfilhoslv@gmail.com>
2023-08-23 19:57:51 -0300

    feat: new icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,22 @@ export default function Home() {
   return (
     <main className="max-w-6xl m-auto my-6 rounded-md flex flex-col md:flex-row">
       <header className="flex flex-col self-start gap-8 md:w-[850px]">
-        <div className="h-24 w-24 rounded bg-gradient-to-r from-purple-600 to-blue-600" />
+        <div className="h-24 w-24 rounded bg-gradient-to-r from-blue-600 to-green-600" />
         <div className="space-y-3">
           <h1 className="text-3xl font-extrabold mb-0">
             <Balancer>I&apos;m Cesar, a Front-end Developer.</Balancer>
           </h1>
-          <p className="text-gray-500">
-            I like to learn new technologies and create applications.
+          <p className="text-gray-600">
+            I am aI like to learn new technologies and create applications.
           </p>
+          <div className="flex gap-4">
+            <Link target="_blank" href="https://twitter.com/cesardotdev">
+              <Icons.twitter className="w-5 h-5 text-gray-600 hover:text-gray-900" />
+            </Link>
+            <Link target="_blank" href="https://github.com/cesarsdcf">
+              <Icons.github className="w-5 h-5 text-gray-600 hover:text-gray-900" />
+            </Link>
+          </div>
         </div>
       </header>
 
@@ -35,7 +43,7 @@ export default function Home() {
                 <div className="flex-col sm:flex-row gap-2 mt-4 flex items-start sm:items-center justify-center sm:justify-between">
                   <div className="flex items-center justify-start gap-4">
                     <Link href={"/"}>
-                      <Icons.gitHub className="w-4 h-4 hover:scale-110" />
+                      <Icons.github className="w-4 h-4 hover:scale-110" />
                     </Link>
                     <Link href={"/"}>
                       <Icons.externallink className="w-4 h-4 hover:scale-110" />
@@ -63,7 +71,7 @@ export default function Home() {
                 <div className="flex-col sm:flex-row gap-2 mt-4 flex items-start sm:items-center justify-center sm:justify-between">
                   <div className="flex items-center justify-start gap-4">
                     <Link href={"/"}>
-                      <Icons.gitHub className="w-4 h-4 hover:scale-110" />
+                      <Icons.github className="w-4 h-4 hover:scale-110" />
                     </Link>
                     <Link href={"/"}>
                       <Icons.externallink className="w-4 h-4 hover:scale-110" />
@@ -91,7 +99,7 @@ export default function Home() {
                 <div className="flex-col sm:flex-row gap-2 mt-4 flex items-start sm:items-center justify-center sm:justify-between">
                   <div className="flex items-center justify-start gap-4">
                     <Link href={"/"}>
-                      <Icons.gitHub className="w-4 h-4 hover:scale-110" />
+                      <Icons.github className="w-4 h-4 hover:scale-110" />
                     </Link>
                     <Link href={"/"}>
                       <Icons.externallink className="w-4 h-4 hover:scale-110" />
@@ -119,7 +127,7 @@ export default function Home() {
                 <div className="flex-col sm:flex-row gap-2 mt-4 flex items-start sm:items-center justify-center sm:justify-between">
                   <div className="flex items-center justify-start gap-4">
                     <Link href={"/"}>
-                      <Icons.gitHub className="w-4 h-4 hover:scale-110" />
+                      <Icons.github className="w-4 h-4 hover:scale-110" />
                     </Link>
                     <Link href={"/"}>
                       <Icons.externallink className="w-4 h-4 hover:scale-110" />
@@ -147,7 +155,7 @@ export default function Home() {
                 <div className="flex-col sm:flex-row gap-2 mt-4 flex items-start sm:items-center justify-center sm:justify-between">
                   <div className="flex items-center justify-start gap-4">
                     <Link href={"/"}>
-                      <Icons.gitHub className="w-4 h-4 hover:scale-110" />
+                      <Icons.github className="w-4 h-4 hover:scale-110" />
                     </Link>
                     <Link href={"/"}>
                       <Icons.externallink className="w-4 h-4 hover:scale-110" />
