74d2c529a5018422bb4cbc9b2d325b42b54383b7
Cesar Silva <cesarfilhoslv@gmail.com>
2023-12-04 13:47:04 -0300

    feat: Added email card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import { Balancer } from "react-wrap-balancer"
 import { Project } from "@/components/project";
 import { ModeToggle } from "@/components/theme-toggle";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { GitHubLogoIcon, LinkedInLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import { buttonVariants } from "@/components/ui/button";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { siteConfig } from "@/lib/config";
-import { Badge, badgeVariants } from "@/components/ui/badge";
-import { AlertCircle } from "lucide-react";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { badgeVariants } from "@/components/ui/badge";
+import { ContactButton } from "@/components/contact-button";
 
 export default function Home() {
 
@@ -25,14 +24,8 @@ export default function Home() {
           <p>
             Comecei a construir um projeto open-source chamado <Link target="_blank" href="https://bidshop.vercel.app/" className={cn(badgeVariants({variant: "secondary"}), "p-1 no-underline")} >Bidshop</Link>, uma plataforma de venda e compra online de produtos, onde é possível enviar ofertas para o vendedor e conversar com ele em tempo real através de um chat dentro do aplicativo.
           </p>
-          <Alert variant={"destructive"}>
-            <AlertCircle className="h-4 w-4" />
-            <AlertTitle className="text-base">Atenção!</AlertTitle>
-            <AlertDescription>
-              Estou em busca de novas oportunidades caso queria entrar em contato clique nesse card.
-            </AlertDescription>
-          </Alert>
-
+     
+          <ContactButton/>
         </div>
         <div className="flex flex-col md:flex-row items-center gap-4 self-start">
           <Link target="_blank" href={siteConfig.links.linkedin} className={cn(buttonVariants({variant: "outline", size:"icon" }))}>
@@ -47,7 +40,7 @@ export default function Home() {
           <ModeToggle />
         </div>
       </section>
-      <section className="mt-10">
+      <section className="mt-6">
         <h2 className="text-xl font-medium mb-5">Projetos</h2>
         <ul className="flex flex-col space-y-7">
           {siteConfig.projects.map((project, i) => (
@@ -62,8 +55,21 @@ export default function Home() {
             <h1 className="text-gray-500">No projects...</h1>
           )}
         </ul>
+        {/* <Link
+              href="/"
+              className={cn(
+                buttonVariants({
+                  variant: 'outline',
+                  className: 'mx-auto flex w-fit',
+                }),
+              )}
+            >
+              Ver mais
+              <ArrowRightIcon className="ml-2 h-4 w-4" aria-hidden="true" />
+              <span className="sr-only">Ver mais</span>
+        </Link> */}
       </section>
-      <section className="mt-10">
+      <section className="mt-8">
         <h2 className="text-xl font-medium mb-5">Works</h2>
       </section>
     </main>
---
+++ src/components/contact-button.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { AlertCircle } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import Link from "next/link";
+
+export function ContactButton() {
+    return (
+        <Link href="mailto:cesarfilhoslv@gmail.com" className="no-underline block">
+            <Alert variant={"destructive"}>
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle className="text-base">Atenção!</AlertTitle>
+                <AlertDescription>
+                Estou em busca de novas oportunidades caso queria entrar em contato clique nesse card.
+                </AlertDescription>
+            </Alert>
+        </Link>
+    )
+}
