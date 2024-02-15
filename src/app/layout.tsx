7712dd4228cc14fd1d588cc2c105b48ccfd301b3
cesarsfilho <cesarscfilho@gmail.com>
2024-02-15 12:49:38 -0300

    add sitemap/robots/and og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,55 @@
-import { cn } from "@/lib/utils";
+import { absoluteUrl, cn } from "@/lib/utils";
 import "./globals.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { fontSans } from "@/lib/fonts";
 import { siteConfig } from "@/lib/config";
 import { AlertBar } from "@/components/alertbar";
+import { env } from "@/env.mjs";
 
 export const metadata: Metadata = {
-  title: siteConfig.name,
-  description: siteConfig.description
-};
+  metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "nextjs",
+    "react",
+    "react server components",
+    "cesar silva"
+  ],
+  authors: [
+    {
+      name: "cesarsilva",
+      url: "https://www.cesarsilva.site/",
+    },
+  ],
+  creator: "cesarsilva",
+  openGraph: {
+    type: "website",
+    locale: "pt-BR",
+    url:siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  manifest: absoluteUrl("/site.webmanifest"),
+}
 
+export const viewport: Viewport = {
+  colorScheme: "dark light",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+}
 export default function RootLayout({
   children,
 }: {
